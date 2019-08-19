0aaefcfc9a7f335227b5527ea658a0b570bb87f5
mugen tsukuyomi <guilhermesamuel79@gmail.com>
2019-08-19 21:48:37 +0000

    construção do javadoc da classe

--- Projeto/Contador.java
+++ Projeto/Contador.java
@@ -1,9 +1,19 @@
+/*
+ * A classe Contador armazena uma contagem para auxiliar a classe CampoEstatistica 
+   */
+
 import java.awt.Color;
 
 public class Contador
 {
     private String name;
     private int count;
+    
+     /*
+     * Construtor da classe Contador que recebe o nome do 
+     * contador como parâmetro
+     * @param nome do contador.
+       */
 
     public Contador(String name)
     {
@@ -11,21 +21,35 @@ public class Contador
         count = 0;
     }
     
+    /*
+     * Captura o nome do contador
+     * @return name
+       */
     public String getName()
     {
         return name;
     }
-
+    
+    /*
+     * Captura o valor da contagem atual do contador
+     * @return count
+       */
     public int getCount()
     {
         return count;
     }
 
+    /*
+     * Incrementa o valor da contagem atual em uma unidade
+       */
     public void increment()
     {
         count++;
     }
     
+    /*
+     * Reseta a contagem atual, atribuindo o valor 0 à variável count
+       */
     public void reset()
     {
         count = 0;
