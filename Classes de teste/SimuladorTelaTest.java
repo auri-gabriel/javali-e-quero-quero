0ad6fab2107b5855d1dc30072104b842ce3b263d
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-09-17 20:35:56 +0000

    Update da classe de teste do SimuladorTela

--- Classes de teste/SimuladorTelaTest.java
+++ Classes de teste/SimuladorTelaTest.java
@@ -38,19 +38,25 @@ public class SimuladorTelaTest {
 			System.out.println("Erro: " + exception);
 		}
 	}
+
 	
 	@Test
 	public void testSetCor() {
 		try {
 			System.out.println("Teste do método setCor");
-			System.out.println(" ");
+			System.out.println("...");
 			Field colors = simulator.getClass().getDeclaredField("cores");
 			colors.setAccessible(true);
+			Method getCor = simulator.getClass().getDeclaredMethod("getCor", Class.class);
+			getCor.setAccessible(true);
+			Color corJavali = (Color) getCor.invoke(simulator, Javali.class);
+			System.out.println("Cor do javali antes: " + corJavali);
 			simulator.setCor(Javali.class, Color.red);
 			@SuppressWarnings("unchecked")
 			Map <Class <?>, Color> colorsMap = (Map<Class<?>, Color>) colors.get(simulator);
 			Color color = colorsMap.get(Javali.class);
 			Assert.assertEquals(Color.red.getRGB(), color.getRGB());
+			System.out.println("Cor do javali após: " + color);
 			
 		} catch (Exception exception) {
 			System.out.println("Erro: " + exception);
@@ -61,13 +67,13 @@ public class SimuladorTelaTest {
 	public void testGetCor() {
 		try {
 			System.out.println("Teste do método getCor");
-			System.out.println(" ");
+			System.out.println("...");
 			simulator.setCor(Javali.class, Color.red);
 			Method getCor = simulator.getClass().getDeclaredMethod("getCor", Class.class);
 			getCor.setAccessible(true);
 			Color corJavali = (Color) getCor.invoke(simulator, Javali.class);
-			
 			Assert.assertEquals(Color.red.getRGB(), corJavali.getRGB());
+			System.out.println("Cor da classe javali: " + corJavali.getAlpha());
 			
 		} catch (Exception exception) {
 			System.out.println("Erro:" + exception);
@@ -77,9 +83,10 @@ public class SimuladorTelaTest {
 	@Test
 	public void testEhViavel() {
 		System.out.println("Teste do método ehViavel: ");
-		System.out.println(" ");
+		System.out.println("...");
 		fieldStat = new CampoEstatistica();
 		field = new Campo(100,100);
 		Assert.assertEquals(fieldStat.ehViavel(field), simulator.ehViavel(field));
 		System.out.println("Método executado de forma bem-sucedida");
 	}
+}
