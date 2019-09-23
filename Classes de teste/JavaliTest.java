71836707f64c7569bf6f40a939d1bc13b9de06cc
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2019-09-23 17:32:25 +0000

    Atualização nos testes devido a adição da classe Animal Aumento da cobertura de testes: - Adição do teste morteFome();

--- Classes de teste/JavaliTest.java
+++ Classes de teste/JavaliTest.java
@@ -1,143 +1,163 @@
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import java.lang.reflect.Field;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 
 class JavaliTest {
 
-	static Javali j;
+
 	static Campo c;
-	static Localizacao l;
+	static Localizacao l, l2;
+	static QueroQuero q;
+	static Animal a;
+	private List<Animal> novosJavalis;
+	
 
 	@Before
 	void setUp() throws Exception {
 		c = new Campo(50, 50);
 		l = new Localizacao(26, 23);
-		j = new Javali(false, c, l);
-
+		l2 = new Localizacao(26, 24);
+		a =   new Javali(false, c, l);
+		
 	}
-    // MÉTODO DE TESTE PARA CONFERIR SE A IDADE DO JAVALI ESTÁ AUMENTANTO COM O TEMPO
+
+
 	@Test
 	void testeIdade() {
 
 		try {
-		
-			Field idade = j.getClass().getDeclaredField("idade");
+
+			setUp();
+			Field idade = a.getClass().getDeclaredField("idade");
 			idade.setAccessible(true);
 			System.out.println("===TEST:1===");
-			System.out.println("idade: " + idade.get(j));
-			Method metodo = j.getClass().getDeclaredMethod("incrementaIdade");
+			System.out.println("idade: " + idade.get(a));
+			Method metodo = a.getClass().getDeclaredMethod("incrementaIdade");
 			metodo.setAccessible(true);
-			metodo.invoke(j);
-			Assert.assertEquals(1, idade.get(j));
-			System.out.println("idade: " + idade.get(j));
+			metodo.invoke(a);
+			Assert.assertEquals(1, idade.get(a));
+			System.out.println("idade: " + idade.get(a));
 		} catch (Exception e) {
-			System.out.print("" + e);
-			;
+			
+			
 		}
 
 	}
-    // TESTE PARA CONFERIR SE O NIVEL DE FOME DO JAVALI ESTÁ BAIXANDO
+
 	@Test
 	void testeFome() {
 		try {
-	
-			Field fome = j.getClass().getDeclaredField("nivelFome");
+			setUp();
+			Field fome = a.getClass().getDeclaredField("nivelFome");
 			fome.setAccessible(true);
-			System.out.println("===TEST:2===");
-			System.out.println("Fome: " + fome.get(j));
-			Method metodo = j.getClass().getDeclaredMethod("incrementaFome");
-			metodo.setAccessible(true);
-			metodo.invoke(j);
-			
-			Assert.assertEquals(6, fome.get(j));
-			System.out.println("Fome: "+fome.get(j));
+			Assert.assertEquals(7, fome.get(a));
+			Method incrementaFome = a.getClass().getDeclaredMethod("incrementaFome");
+			incrementaFome.setAccessible(true);
+			incrementaFome.invoke(a);
+			Assert.assertEquals(6, fome.get(a));
+
 		} catch (Exception e) {
-			System.out.print("" + e);
 			
-		}
 
-	}
-	// TESTE APRA VER O STATUS DE VIDA DO JAVALI
-	@Test
-	void testEstaVivo() {
-		Assert.assertTrue(j.estaVivo());
-	}
-    // TESTE PARA VERIFICAR A LOCALIZAÇÃO DO JAVALI
-	@Test
-	void testGetLocalizacao() {
-		Assert.assertEquals("23,26", j.getLocalizacao());
-	}
-	// MÉTODO DE TESTE PARA VERIFICAR SE O JAVALI ESTÁ NA IDADE DE PROCIRAÇÃO
-	@Test
-	void testepodeProcria() {
-		try {
-			Field idade = j.getClass().getDeclaredField("idade");
-			idade.setAccessible(true);
-			idade.setInt(j, 78);
-			System.out.println("===TEST:3===");
-			System.out.println("idade: "+idade.get(j));
-			Method metodo = j.getClass().getDeclaredMethod("podeProcriar");
-			metodo.setAccessible(true);
-			Assert.assertTrue((boolean) metodo.invoke(j));
-			System.out.println("Já está na idade de procriação");
-
-			
-			}
-		catch(Exception e) {
-		System.out.print(""+e);
 		}
 		
 	}
-	// TESTE DO MÉTODO SETMORTE 
+	
+	@Test 
+	void morteFome(){
+		try {
+			setUp();
+			Field fome = a.getClass().getDeclaredField("nivelFome");
+			fome.setAccessible(true);
+			fome.set(a, 1);
+			Assert.assertEquals(1, fome.get(a));
+			Method incrementaFome = a.getClass().getDeclaredMethod("incrementaFome");
+			incrementaFome.setAccessible(true);
+			incrementaFome.invoke(a);
+			Assert.assertEquals(0, fome.get(a));
+			Assert.assertFalse(a.estaVivo());
+		} catch (Exception e) {
+			
+
+		}
+	}
+
+	
+	@Test
+	void testeProcria() {
+		try {
+
+			setUp();
+			Field idade = a.getClass().getDeclaredField("idade");
+			idade.setAccessible(true);
+			idade.setInt(a, 17);
+			System.out.println("===TEST:5===");
+			Method MPP = a.getClass().getDeclaredMethod("podeProcriar");
+			MPP.setAccessible(true);
+			MPP.invoke(a);
+			Method MP = a.getClass().getDeclaredMethod("procria");
+			MP.setAccessible(true);
+			int returnNascimentos = (int) MP.invoke(a);
+			System.out.println("Nascimentos: " + returnNascimentos);
+			Assert.assertNotNull(returnNascimentos);
+		} catch (Exception e) {
+
+		}
+	}
+
+	@Test
+	void testeProcuraComida() {
+		try {
+			Field fome = a.getClass().getDeclaredField("nivelFome");
+			fome.setAccessible(true);
+			fome.set(a, 3);
+			Method metodo = a.getClass().getDeclaredMethod("procuraComida");
+			metodo.setAccessible(true);
+			metodo.invoke(a);
+			Assert.assertEquals(7, fome.get(a));
+
+		} catch (Exception e) {
+
+		}
+	}
+
 	@Test
 	void testeMorte() {
 		try {
-	
-			Field vivo = j.getClass().getDeclaredField("vivo");
+			setUp();
+			Field vivo = a.getClass().getDeclaredField("vivo");
 			vivo.setAccessible(true);
-			System.out.println("===TEST:2===");
-			System.out.println("Status:" + vivo.get(j));
-			System.out.println("Localização:" + j.getLocalizacao());
-			Method metodo = j.getClass().getDeclaredMethod("setMorte");
+			Method metodo = a.getClass().getDeclaredMethod("setMorte");
 			metodo.setAccessible(true);
-			metodo.invoke(j);
-			Assert.assertFalse((boolean) vivo.get(j));
-			System.out.println("Status:" + vivo.get(j));
-			Assert.assertEquals(null, j.getLocalizacao());
-			System.out.println("Localização:" + j.getLocalizacao());
+			metodo.invoke(a);
+			Assert.assertFalse((boolean) vivo.get(a));
+			Assert.assertEquals(null, a.getLocalizacao());
+			System.out.println("Localização:" + a.getLocalizacao());
 		} catch (Exception e) {
 			System.out.println("" + e);
 		}
 	}
-    //  TESTE DA CLASSE PROCRIA
-    void testeProcria(){
-		try {
-		
-		
-			Field idade = j.getClass().getDeclaredField("idade");
+	
+	@Test
+    void atividade() {
+    	try {
+    		setUp();
+			Field idade = a.getClass().getDeclaredField("idade");
 			idade.setAccessible(true);
-			idade.setInt(j, 16);
-			System.out.println("==TEST:5===");
-			Method MPP = j.getClass().getDeclaredMethod("podeProcriar");
-			MPP.setAccessible(true);
-			MPP.invoke(j);
-			Method MP = j.getClass().getDeclaredMethod("procria");
-			MP.setAccessible(true);
-			MP.invoke(j);
-			int returnNascimentos = (int) MP.invoke(j);
-			System.out.println("Nascimentos: "+returnNascimentos);
-			Assert.assertNotNull(returnNascimentos);
-		}
-		catch(Exception e) {
-			
+			idade.set(a, 19);
+			Field fome = a.getClass().getDeclaredField("nivelFome");
+			fome.setAccessible(true);
+			a.atividade(novosJavalis);
+			Assert.assertTrue(a.estaVivo());
+		} catch (Exception e) {
+			System.out.println("" + e);
 		}
 	}
-
-
 
 }
