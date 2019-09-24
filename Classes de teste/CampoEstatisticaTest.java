f0392125a852e89bfbe019bb51747235fb2cc12c
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-09-24 14:04:58 +0000

    Update da classe de teste CampoEstatistica (adição dos métodos EhViavel e ObterDetalhesPopulacao)

--- Classes de teste/CampoEstatisticaTest.java
+++ Classes de teste/CampoEstatisticaTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import classes.Campo;
 import classes.CampoEstatistica;
 import classes.Contador;
+import java.util.HashMap;
 
 public class CampoEstatisticaTest {
 	
@@ -19,11 +20,25 @@ public class CampoEstatisticaTest {
 		System.out.println("===================");
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testObterDetalhesPopulacao() {
 		try {
 			System.out.println("Teste do método obterDetalhesPopulacao:");
 			System.out.println(" ");
+			Campo field = new Campo(100, 100);
+			Field counters = fieldStat.getClass().getDeclaredField("contadores");
+			counters.setAccessible(true);
+			HashMap <Class<?>, Contador> counterMap = (HashMap<Class<?>, Contador>) counters.get(fieldStat); 
+			StringBuffer info = new StringBuffer();
+			for(Class<?> key : counterMap.keySet()) {
+	            Contador count = counterMap.get(key);
+	            info.append(count.getNome());
+	            info.append(": ");
+	            info.append(count.getContagem());
+	            info.append(' ');
+	        }
+			Assert.assertTrue(fieldStat.obterDetalhesPopulacao(field).contentEquals(info));
 			
 		} catch (Exception e) {
 			System.out.println("Erro: " + e);
@@ -81,13 +96,22 @@ public class CampoEstatisticaTest {
 		
 	}
 
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testEhViavel() {
 		try {
 			System.out.println("Teste do método ehViavel");
 			System.out.println(" ");
+			Campo field = new Campo(100, 100);
 			Field counters = fieldStat.getClass().getDeclaredField("contadores");
 			counters.setAccessible(true);
+			HashMap <Class<?>, Contador> counterMap = (HashMap<Class<?>, Contador>) counters.get(fieldStat); 
+			int entire = 0;
+			for(Class<?> key : counterMap.keySet()) {
+	            Contador info = counterMap.get(key);
+	            entire = info.getContagem();
+	        }
+			Assert.assertEquals(fieldStat.ehViavel(field), entire > 1);
 			
 		} catch (Exception e) {
 			System.out.println("Erro: " + e);
