b47e6cd4622bf12349ec96956676e0598e9a5178
Henrique Hoff de Lima <henriquehoffdelima@gmail.com>
2019-09-24 15:37:28 +0000

    Aumento na cobertura e rafatoração da classe de teste simulador: - mudanças nos testes em função da adição da classe Animal;

--- Classes de teste/SimuladorTest.java
+++ Classes de teste/SimuladorTest.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,44 +15,39 @@ class SimuladorTest {
 	
 	
 	@Test
-	void testSimulacao() {
-		try {
-			System.out.println("Teste do método simulacao: ");
-			System.out.println("===========================");
+	void testSimulacao() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		
+			
 			Field stage = simulator.getClass().getDeclaredField("etapa");
 			stage.setAccessible(true);
-			System.out.println("Número de etapas antes da chamada ao método: " + stage.get(simulator));
+		
 			simulator.simulacao(50);
-			System.out.println("Número de etapas após a chamada ao método: " + stage.get(simulator));
+		
 			Assert.assertEquals(50, stage.get(simulator));
 			System.out.println(" ");
 		}
-		catch (Exception exception) {
-			System.out.println("Erro: " + exception);
-		}
-	}
+	
+		
+	
 
 	@Test
-	void testExecutaLongaSimulacao() {
-		try {
-			System.out.println("Teste do método executaLongaSimulacao: ");
-			System.out.println("=======================================");
+	void testExecutaLongaSimulacao() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		
+		
 			Field stage = simulator.getClass().getDeclaredField("etapa");
 			stage.setAccessible(true);
-			System.out.println("Número de etapas antes da chamada ao método: " + stage.get(simulator));
+			Assert.assertEquals(0, stage.get(simulator));
 			simulator.executaLongaSimulacao();
-			System.out.println("Número de etapas após a chamada ao método: " + stage.get(simulator));
-			Assert.assertEquals(500, stage.get(simulator));
-			System.out.println(" ");
-		} 
-		catch(Exception exception) {
-			System.out.println("Erro: " + exception);
-		}
+			int etapa = stage.getInt(simulator);
+			Assert.assertEquals(etapa, stage.get(simulator));
+			Assert.assertNotEquals(0, stage.get(simulator));
+
+	
 	}
 	
 	@Test 
-	void testSimulacaoUmaEtapa() {
-		try {
+	void testSimulacaoUmaEtapa() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	
 			System.out.println("Teste do método simulacaoUmaEtapa: ");
 			System.out.println("===================================");
 			Field stage = simulator.getClass().getDeclaredField("etapa");
@@ -61,67 +57,36 @@ class SimuladorTest {
 			System.out.println("Número de etapas após a chamada ao método: " + stage.get(simulator));
 			Assert.assertEquals(1, stage.get(simulator));
 			System.out.println(" ");
-		}
-		catch (Exception exception) {
-			System.out.println("Erro: " + exception);
-		}
+
 	}
 	
 	@Test
-	void testReiniciaSimulacao() {
-		try {
-			System.out.println("Teste do método reiniciaSimulacao: ");
-			System.out.println("===================================");
+	void testReiniciaSimulacao() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+	
+			List<Animal> animais =  new ArrayList<Animal>();
 			Field stage = simulator.getClass().getDeclaredField("etapa");
 			stage.setAccessible(true);
-			Field queroQueros = simulator.getClass().getDeclaredField("queroQueros");
-			queroQueros.setAccessible(true);
-			Field javalis = simulator.getClass().getDeclaredField("javalis");
-			javalis.setAccessible(true);
 			simulator.simulacao(100);
 			simulator.povoa();
-			List<QueroQuero> qtdQueroQueros = (List<QueroQuero>) queroQueros.get(simulator);
-			List<Javali> qtdJavalis = (List<Javali>) javalis.get(simulator);
-			System.out.println("Número de etapas antes da chamada ao método: " + stage.get(simulator));
-			System.out.println("Número de quero-queros antes da chamada ao método: " + qtdQueroQueros.size());
-			System.out.println("Número de javalis após a chamada ao método: " + qtdJavalis.size());
+			Assert.assertNotNull(animais.size());
+		
 			simulator.reiniciaSimulacao();
-			System.out.println("Número de etapas após a chamada ao método: " + stage.get(simulator));
-			System.out.println("Número de quero-queros após a chamada ao método: " + qtdQueroQueros.size());
-			System.out.println("Número de javalis após a chamada ao método: " + qtdJavalis.size());
+
 			Assert.assertEquals(0, stage.get(simulator));
-			Assert.assertEquals(0, qtdJavalis.size());
-			Assert.assertEquals(0, qtdQueroQueros.size());
-			System.out.println(" ");
-		}
-		catch (Exception exception) {
-			System.out.println("Erro: " + exception);
-		}
+			Assert.assertEquals(0, animais.size());
+		
+	
 	}
 	
 	@Test
 	void testPovoa() {
-		try {
-			System.out.println("Teste do método povoa: ");
-			System.out.println("=======================");
-			Field queroQueros = simulator.getClass().getDeclaredField("queroQueros");
-			queroQueros.setAccessible(true);
-			Field javalis = simulator.getClass().getDeclaredField("javalis");
-			javalis.setAccessible(true);
-			List<QueroQuero> qtdQueroQueros = (List<QueroQuero>) queroQueros.get(simulator);
-			List<Javali> qtdJavalis = (List<Javali>) javalis.get(simulator);
-			System.out.println("Número de quero-queros antes da chamada ao método: " + qtdQueroQueros.size());
-			System.out.println("Número de javalis após a chamada ao método: " + qtdJavalis.size());
+	
+			List<Animal> animais =  new ArrayList<Animal>();
+			Assert.assertEquals(0,animais.size());
 			simulator.povoa();
-			System.out.println("Número de quero-queros após a chamada ao método: " + qtdQueroQueros.size());
-			System.out.println("Número de javalis após a chamada ao método: " + qtdJavalis.size());
-			Assert.assertNotNull(queroQueros.get(simulator));
-			Assert.assertNotNull(javalis.get(simulator));
-			System.out.println("População criada com sucesso!");
-			System.out.println(" ");
-		}
-		catch (Exception exception) {
-			System.out.println("Erro: " + exception);
-		}
+			Assert.assertNotNull(animais.size());
+		
+	
+
 	}
 }
