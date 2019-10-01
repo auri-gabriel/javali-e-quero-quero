fe0fc0dfc302a7705039052af95060e17535948f
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-10-01 19:01:34 +0000

    Atualização da classe de teste Randomizador

--- Classes de teste/RandomizadorTest.java
+++ Classes de teste/RandomizadorTest.java
@@ -1,19 +1,9 @@
-package teste;
-
 import classes.Randomizador;
-import java.lang.reflect.Field;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 
 public class RandomizadorTest {
-
-	private Randomizador random;
-	@Before
-	public void setUp() throws Exception {
-		random = new Randomizador();
-	}
 
 	@Test
 	public void testGetRandomDefault() {
@@ -22,15 +12,12 @@ public class RandomizadorTest {
 			ArrayList<Double> actual = new ArrayList<>();
 			
 			for (int i = 0; i < 5; i++) {
-				expected.add(Randomizador.getRandom().nextDouble());
-			}
+				expected.add(Randomizador.getRandom().nextDouble());}
+			
 			
 			for(int i = 0; i < 5; i++) {
-				actual.add(Randomizador.getRandom().nextDouble());
-			}
-			
-			System.out.println("=================");
-			
+				actual.add(Randomizador.getRandom().nextDouble());}
+						
 			Assert.assertFalse(expected.containsAll(actual));
 			
 		} catch (Exception exception) {
@@ -44,16 +31,13 @@ public class RandomizadorTest {
 			ArrayList<Double> expected = new ArrayList<>();
 			ArrayList<Double> actual = new ArrayList<>();
 			
-			for(int j = 0; j < 5; j++) {
-				actual.add(Randomizador.getRandom().nextDouble());
-			}
-						
 			Randomizador.reset();
-						
-			for(int i = 0; i < 6; i++) {
-				expected.add(Randomizador.getRandom().nextDouble());
-			}
-
+			for(int j = 0; j < 5; j++) {
+				actual.add(Randomizador.getRandom().nextDouble());}
+			
+			Randomizador.reset();
+			for(int i = 0; i < 5; i++) {
+				expected.add(Randomizador.getRandom().nextDouble());}
 			
 			Assert.assertTrue(expected.containsAll(actual));
 			
