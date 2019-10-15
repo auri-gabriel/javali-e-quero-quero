61db12bb692ca06ecc0dcb23741a8b366df8a8f3
Guilherme Samuel <guilhermesamuel79@gmail.com>
2019-10-15 07:05:51 +0000

    Update da aplicação da interface gráfica

--- Interface/App.java
+++ Interface/App.java
@@ -1,26 +1,35 @@
-package userinterface;
+package interfaces;
 
+import java.awt.Color;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import classes.Simulador;
 
-public class App implements ActionListener {
+public class App implements ActionListener{
 
-	private Simulador simulator;
 	private JFrame window;
 	private JTextField txtLine, txtColumn, txtStage;
-	private JLabel lblLine, lblColumn, lblStage;
-	private JButton btnConfirm;
+	private JLabel lblLine, lblColumn, lblStage, lblOtherOptions, lblImage;
+	private JButton btnConfirm, btnOtherOptions;
+	private JComboBox<String> cbOptions;
+	private ImageIcon image = new ImageIcon(getClass().getResource("mapa (1).png"));
+	private int line, column, stage;
 	
 	public void run() {
         window = new JFrame("Simulador Javali e Quero-Quero");
         window.setSize(500, 500);
         window.setLayout(null);
-        addComponents();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setBackground(Color.blue);
+        addComponents(); 
+        setIcon();
         window.setVisible(true);
 	}
 	
@@ -29,12 +38,16 @@ public class App implements ActionListener {
         int y = 20;
         int dimX = 100;
         int dimY = 40;
+        
+        lblImage = new JLabel(image);
+        lblImage.setBounds(x + dimX + dimX * 2 + x, y + 10, 128, 128);
+        window.getContentPane().add(lblImage);
                 
-        lblLine = new JLabel("Insira o n�mero de linhas");
+        lblLine = new JLabel("Insira o número de linhas");
         lblLine.setBounds(x, y, dimX + dimY * 2, dimY);
         window.getContentPane().add(lblLine);  	
         
-        lblColumn = new JLabel("Insira o n�mero de colunas");
+        lblColumn = new JLabel("Insira o número de colunas");
         lblColumn.setBounds(x, y + dimY, dimX + dimY * 2, dimY);
         window.getContentPane().add(lblColumn);
         
@@ -46,7 +59,7 @@ public class App implements ActionListener {
         txtColumn.setBounds(x + dimX * 2, y + 50, dimX, dimY - x);
         window.getContentPane().add(txtColumn);
         
-        lblStage = new JLabel("Insira o n�mero de etapas");
+        lblStage = new JLabel("Insira o número de etapas");
         lblStage.setBounds(x, y + dimY + dimY, dimX + dimY * 2, dimY);
         window.getContentPane().add(lblStage);
         
@@ -54,19 +67,40 @@ public class App implements ActionListener {
         txtStage.setBounds(x + dimX * 2, y + y + 70, dimX, dimY - x);
         window.getContentPane().add(txtStage);  
         
+        btnOtherOptions = new JButton("Executar");
+        btnOtherOptions.setBounds(x + dimX * 2,  y + dimY * 8, dimX + dimY, dimY);
+        btnOtherOptions.addActionListener(this);
+        window.getContentPane().add(btnOtherOptions);
+        
         btnConfirm = new JButton("Gerar simulador");
         btnConfirm.setBounds(x + dimX * 2, y + dimY * 4, dimX + dimY, dimY);
         btnConfirm.addActionListener(this);
         window.getContentPane().add(btnConfirm);
         
+        lblOtherOptions = new JLabel("Outras opções");
+        lblOtherOptions.setBounds(x, y + dimY * 7, dimX + dimY * 2, dimY);
+        window.getContentPane().add(lblOtherOptions);
+        
+        cbOptions = new JComboBox<String>();
+        cbOptions.setBounds(x, y + dimY * 8, dimX * 2, dimY);
+        cbOptions.addItem("Alterar cor de javali");
+        cbOptions.addItem("Alterar cor de quero-quero");
+        cbOptions.addItem("Executar longa simulação");
+        cbOptions.addItem("Reiniciar simulação");
+        window.getContentPane().add(cbOptions);
+              
         window.setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent event) {
-		int line = (int) Double.parseDouble(txtLine.getText());
-		int column = (int) Double.parseDouble(txtColumn.getText());
-		int stage = (int) Double.parseDouble(txtStage.getText());
-		simulator = new Simulador(100, 100);
-		simulator.simulacao(300);
+		line = (int) Double.parseDouble(txtLine.getText());
+		column = (int) Double.parseDouble(txtColumn.getText());
+		stage = (int) Double.parseDouble(txtStage.getText());
+		Simulador simulator = new Simulador(line, column);
+		simulator.simulacao(stage);
+	}
+	
+	private void setIcon() {
+		window.setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("mapa.png")));
 	}
 }
